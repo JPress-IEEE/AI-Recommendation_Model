364a236427f3d6db9ced276c34123cb0ef9d779f
Doaa-Helal <117906221+Doaa-Helal@users.noreply.github.com>
2024-09-22 14:27:51 +0300

    Chore: used system variables to hide sensitive data

--- create_db.py
+++ create_db.py
@@ -1,14 +1,15 @@
 from pymilvus import (
    FieldSchema, DataType, 
    CollectionSchema, Collection,connections)
+from dotenv import load_dotenv, dotenv_values
+import os
 
-ENDPOINT="https://in03-0008120f0b8b227.serverless.gcp-us-west1.cloud.zilliz.com"
+load_dotenv()
+ENDPOINT=os.getenv("milvus_endpoint")
+TOKEN=os.getenv("milvus_token")
 connections.connect(
    uri=ENDPOINT,
-   token="2e021870ed0adebedcf7a869bc5df9905510a5d53114ee7aff6fd8f08d7799bb511c6de82f13d3c4bf45740ca0f0d4d26c0fdcb7")
-
-
-
+   token=TOKEN)
 ## 1. Define a minimum expandable schema.
 fields = [
    FieldSchema("vector", DataType.FLOAT_VECTOR, dim=1024),
@@ -22,7 +23,7 @@ schema = CollectionSchema(
 )
 
 ## 2. Create a collection.
-mc = Collection("jp3", schema)
+mc = Collection("jp4", schema)
 
 ## 3. Index the collection.
 mc.create_index(
